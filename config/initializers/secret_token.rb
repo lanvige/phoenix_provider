6ed011e66ed7373c70a50b086972c16a68be795a
Lanvige Jiang <lanvige@gmail.com>
2012-05-07 20:02:27 +0800

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhoenixProvider::Application.config.secret_token = '34a70333161da9daf3e5f4028cd7e8d383fdeca18bfe940c47ade3c4d4f9243cb537297de33bbe3893e7f9c6832a62ac97354716b43b010c66b12dfc8148bd1b'
